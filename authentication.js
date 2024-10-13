522dc9cb3b07f45649c76dd3636204cfe72102c3
Salim Sofinia <118753876+salimsofinia@users.noreply.github.com>
2024-10-13 14:33:51 +0200

    Authentication update

--- authentication.js
+++ authentication.js
@@ -1,9 +1,162 @@
-const express = require("express");
-const app = express();
-const User = require("./models/user");
-const mongoose = require("mongoose");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const session = require("express-session");
+// const bcrypt = require("bcrypt");
+// const User = require("./models/user");
 
+// const app = express();
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://salimsofinia:Salim123@backenddb.4f0qd.mongodb.net/Share2Teach?retryWrites=true&w=majority&appName=BackendDB",
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }
+//   )
+//   .then(() => {
+//     console.log("MongoDB Connected!");
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB Connection Error:", err);
+//   });
+
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   session({
+//     secret: "your_secret_key",
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
+
+// const requireLogin = (req, res, next) => {
+//   if (!req.session.user_id) {
+//     console.log("No user_id in session");
+//     return res.redirect("/login");
+//   }
+//   next();
+// };
+
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
+
+// app.get("/login", (req, res) => {
+//   res.render("login");
+// });
+
+// app.get("/", requireLogin, async (req, res) => {
+//   if (!req.session.user_id) {
+//     return res.redirect("/login");
+//   }
+//   try {
+//     const user = await User.findById(req.session.user_id);
+//     if (!user) {
+//       req.session.destroy(() => {
+//         res.redirect("/login");
+//       });
+//     } else {
+//       res.render("home", { user: user });
+//     }
+//   } catch (err) {
+//     console.error("Error fetching user data:", err);
+//     res.redirect("/login");
+//   }
+// });
+
+// //
+
+// app.get("/faq", (req, res) => {
+//   res.render("faq");
+// });
+
+// app.get("/registerrole", requireLogin, (req, res) => {
+//   if (!req.session.user_id) {
+//     return res.redirect("/login");
+//   }
+//   User.findById(req.session.user_id, (err, user) => {
+//     if (err || !user) {
+//       req.session.destroy(() => {
+//         res.redirect("/login");
+//       });
+//     } else if (user.credential !== "Admin") {
+//       res.redirect("/");
+//     } else {
+//       res.render("registerrole");
+//     }
+//   });
+// });
+
+// //
+
+// app.post("/register", async (req, res) => {
+//   try {
+//     const { firstname, lastname, email, affiliation, credential, password } =
+//       req.body;
+//     const user = new User({
+//       firstname,
+//       lastname,
+//       email,
+//       affiliation,
+//       credential,
+//       password,
+//     });
+//     await user.save();
+//     res.redirect("/login");
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Registration failed. Please try again.");
+//   }
+// });
+
+// app.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     const foundUser = await User.findAndValidate(email, password);
+//     if (foundUser) {
+//       req.session.user_id = foundUser._id;
+//       console.log("Session user_id set:", email);
+//       res.redirect("/");
+//     } else {
+//       console.log("Invalid email or password");
+//       res.redirect("/login");
+//     }
+//   } catch (err) {
+//     console.error("Error during login:", err);
+//     res.redirect("/login");
+//   }
+// });
+
+// app.post("/", (req, res) => {
+//   req.session.destroy();
+//   res.redirect("/login");
+// });
+
+// app.post("/logout", (req, res) => {
+//   req.session.destroy(() => {
+//     res.redirect("/login");
+//   });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000!");
+// });
+
+const express = require("express");
+const mongoose = require("mongoose");
 const session = require("express-session");
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 mongoose
   .connect(
@@ -14,18 +167,20 @@ mongoose
     }
   )
   .then(() => {
-    console.log("MONGO CONNECTION OPEN!!!");
+    console.log("MongoDB Connected!");
   })
   .catch((err) => {
-    console.log("OH NO MONGO CONNECTION ERROR!!!!");
-    console.log(err);
+    console.error("MongoDB Connection Error:", err);
   });
 
-app.set("view engine", "ejs");
-app.set("views", "views");
-
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: "not a good secret" }));
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const requireLogin = (req, res, next) => {
   if (!req.session.user_id) {
@@ -34,64 +189,75 @@ const requireLogin = (req, res, next) => {
   next();
 };
 
-app.get("/register", (req, res) => {
-  res.render("register");
+app.get("/faq", (req, res) => {
+  res.render("faq");
+});
+
+app.get("/registerrole", requireLogin, async (req, res) => {
+  const user = await User.findById(req.session.user_id);
+  if (user && user.credential === "Admin") {
+    res.render("registerrole");
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  if (req.session.user_id) {
+    res.redirect("/"); // Redirect to home if already logged in
+  } else {
+    res.render("login");
+  }
 });
 
-app.get("/homepage", (req, res) => {
-  res.render("homepage");
+app.get("/register", (req, res) => {
+  if (req.session.user_id) {
+    res.redirect("/"); // Redirect to home if already logged in
+  } else {
+    res.render("register");
+  }
 });
 
-app.get("/homepage", requireLogin, (req, res) => {
-  // if (!req.session.user_id) {
-  //   res.redirect("/login");
-  // }
-  res.render("homepage");
-  //res.send("THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN.");
+app.get("/", async (req, res) => {
+  let user = null;
+  if (req.session.user_id) {
+    user = await User.findById(req.session.user_id);
+  }
+  res.render("home", { user: user });
 });
-
-app.post("/register", async (req, res) => {
-  const { password, username } = req.body;
-  // const hash = await bcrypt.hash(password, 12);
-  const user = new User({ username, password });
-  await user.save();
-  //res.send(hash);
-  res.redirect("/login");
-});
-
-// app.get("/register-ed", (req, res) => {
-//   res.send("register-ed");
-//   setTimeout(() => {
-//     console.log("5 seconds have passed");
-//   }, 5000);
-//   res.redirect("/login");
-// });
 
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  // const user = await User.findOne({ username });
-  // const validPassword = await bcrypt.compare(password, user.password);
-  const foundUser = await User.findAndValidate(username, password);
-  //if (validPassword) {
+  const { email, password } = req.body;
+  const foundUser = await User.findAndValidate(email, password);
   if (foundUser) {
-    req.session.user_id = user._id;
-    res.send("auth is good");
+    req.session.user_id = foundUser._id;
+    res.redirect("/");
   } else {
-    res.send("auth failed");
     res.redirect("/login");
   }
 });
 
-app.post("/homepage", (req, res) => {
-  req.session.user_id = null;
-  //req.session.destroy();
+app.post("/register", async (req, res) => {
+  const { firstname, lastname, email, affiliation, credential, password } =
+    req.body;
+  const user = new User({
+    firstname,
+    lastname,
+    email,
+    affiliation,
+    credential,
+    password,
+  });
+  await user.save();
   res.redirect("/login");
 });
 
+app.post("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
+});
+
 app.listen(3000, () => {
-  console.log("SERVING YOUR APP!");
+  console.log("Server running on port 3000!");
 });
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  firstname: { type: String, required: [true, "First name cannot be blank"] },
+  lastname: { type: String, required: [true, "Last name cannot be blank"] },
+  email: {
+    type: String,
+    required: [true, "Email address cannot be blank"],
+    unique: true,
+  },
+  affiliation: {
+    type: String,
+    required: [true, "Affiliation cannot be blank"],
+  },
+  credential: { type: String, required: [true, "Credential cannot be blank"] },
+  password: { type: String, required: [true, "Password cannot be blank"] },
+});
+
+userSchema.statics.findAndValidate = async function (email, password) {
+  const foundUser = await this.findOne({ email });
+  if (!foundUser) {
+    console.log("User not found for email:", email);
+    return false;
+  }
+  const isValid = await bcrypt.compare(password, foundUser.password);
+  return isValid ? foundUser : false;
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
